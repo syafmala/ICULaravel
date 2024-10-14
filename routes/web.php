9c96b2d32143ee095eadadc58ebcce54ffbf9405
syarf <syarfeeza@icu.gov.my>
2024-10-14 15:47:55 +0800

    init commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/auth/signin', function () {
+    return view('auth.signin');
+});
