ba4c35013f21b9fba72881053fd154c6bd4c7087
syarf <syarfeeza@icu.gov.my>
2024-10-15 10:37:29 +0800

    1. contohsetup utk nama app, tak baca dari env 2. app nama using param 3. belajar routing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ config('app.name') }}</title>
 </head>
 <body>
-    <h1> Hello ICU!</h1>
+    <h1> Hello, {{ $name ?? "Syarf"}}</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,49 @@ Route::get('/home', function () {
     return view('home');
 });
 
+Route::get('/home/{name}', function ($name) {
+    return view('home', ['name' => $name]);
+});
+
 Route::get('/auth/signin', function () {
     return view('auth.signin');
+});
+
+//Named Route - alias of a route user.profile = /pengguna/profile
+Route::get('/user/profile', function () {
+    return 'New User Profile';
+})->name('user.profile');
+
+//Route Param
+Route::get('/user/{name}', function ($name) {
+    return 'User '.$name;
+});
+
+//Redirect route to named route
+Route::get('/redirect-to-profile', function () {
+    return redirect()->route('user.profile');
+});
+
+//Route Group
+Route::prefix('food')->group(function () {
+
+    Route::get('/details', function (){
+        return 'Food details are following';
+    });
+
+    Route::get('/home', function (){
+    return view('Food home page');
+    });
+});
+
+//Combination of all above (Route, Named Route, Route Param, Route Group, Route Prefix)
+Route::name('job')->prefix('job')->group(function () {
+
+    Route::get('home', function (){
+        return 'Job home page';
+    })->name('.home');
+
+    Route::get('details', function (){
+    return 'Job details are following';
+    })->name('.description');
 });
