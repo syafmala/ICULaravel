1d1033d03db14f88876418becc6a13654a1226c6
syarf <syarfeeza@icu.gov.my>
2024-10-15 15:16:25 +0800

    belajar layout

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('title')
+
+@section('content')
+    <h1>i am about page</h1>
+
+    <button type="button" class="btn btn-info">Click Me</button>
+@endsection
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ config('app.name') }}</title>
-</head>
-<body>
-    <h1> Hello, {{ $name ?? "Syarf"}}</h1>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('title', 'Home')
+
+@section('content')
+    @php($_name = $name ?? "team")
+    
+    @if ($_name == "diba")
+        <p>You are banned</p>
+    @else
+        <h1> Hello, {{ $_name }}</h1>
+     @endif
+
+     <button type="button" class="btn btn-dark">Click Me</button>
+@endsection
+
+
