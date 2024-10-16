a134e3eed1bf75e0cec1d01283f18a632fa41bd0
syarf <syarfeeza@icu.gov.my>
2024-10-16 09:28:03 +0800

    belajar factory dan seeder

---
+++ database/factories/FeedFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class FeedFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->text(100),
+            'description' => fake()->text(),
+            'user_id' => fake()->numberBetween(1,10),
+            'updated_at' => fake()->dateTime(),
+            'created_at' => fake()->dateTime(),
+        ];
+    }
+}
---
+++ database/migrations/2024_10_15_080105_create_feeds_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feeds', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',100);
+            $table->text('description',100);
+
+            $table->foreignId('user_id');
+            $table->foreign('user_id')
+                ->on('users')
+                ->references('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feeds');
+    }
+};
