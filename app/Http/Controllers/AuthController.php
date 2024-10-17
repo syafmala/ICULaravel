481b00d5fb6f3b9ece54e80810d9c3478607612f
syarf <syarfeeza@icu.gov.my>
2024-10-17 17:02:21 +0800

    update changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -44,6 +44,9 @@ class AuthController extends Controller
         ]);
  
         if (Auth::attempt($credentials)) {
+            
+            
+            
             $request->session()->regenerate();
  
             return redirect()->intended('feeds');
